e2e884ae79ea640ecb512f9d13e879093b6425df
RomuloCarlos <romulo.oliveira@fatec.sp.gov.br>
2022-03-16 23:18:08 -0300

    rotas

--- controller/index.js
+++ controller/index.js
@@ -2,7 +2,19 @@ const express= require("express");
 
 const app = express();
 
+app.get("/contato", function (req,resp){
+    resp.send("Pagina de contato do meu app")
+});
+
+app.get("/produto", function (req,resp){
+    resp.send("Pagina de contato do meu produto")
+});
+
+app.get("/dados/:nome/:cargo", function (req,resp){
+    resp.send("<h1> Ola sr(a). "+req.params.nome+"</h1><h2>Seu cargo é " +req.params.cargo+ "</h2>")
+});
+
 //esta sempre deve ser a ultima linha quando usamos o express
-app.listen(8081, function(){
-    console.log("Servidor funcionando na url http://localhost:8081");
+app.listen(8089, function(){
+    console.log("Servidor funcionando na url http://localhost:8089");
 });
